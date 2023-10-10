4081b52069aef4777fabdb0bbdef06fb0cbd3ce6
kangnahyeon <skgus123@ajou.ac.kr>
2023-10-11 01:40:15 +0900

    Change directory name from src to xv6-public

--- initial-xv6/test-getreadcount.sh
+++ initial-xv6/test-getreadcount.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-if ! [[ -d src ]]; then
-    echo "The src/ dir does not exist."
-    echo "Your xv6 code should be in the src/ directory"
+if ! [[ -d xv6-public ]]; then
+    echo "The xv6-public/ dir does not exist."
+    echo "Your xv6 code should be in the xv6-public/ directory"
     echo "to enable the automatic tester to work."
     exit 1
 fi
